701ca7490c2fb222dda846be5edc7e2a93d282e9
Anang <tyoandini@gmail.com>
2021-06-24 08:17:49 +0700

    menyimpan data di method store dan fillable model

--- app/Http/Controllers/StudentsController.php
+++ app/Http/Controllers/StudentsController.php
@@ -36,7 +36,23 @@ class StudentsController extends Controller
      */
     public function store(Request $request)
     {
-        return $request;
+        // $student = new Student();
+        // $student->nama = $request->nama;
+        // $student->nrp = $request->nrp;
+        // $student->email = $request->email;
+        // $student->jurusan = $request->jurusan;
+
+        // $student->save();
+
+        // Student::create([
+        //     'nama' => $request->nama,
+        //     'nrp' => $request->nrp,
+        //     'email' => $request->email,
+        //     'jurusan' => $request->jurusan
+        // ]);
+
+        Student::create($request->all());
+        return redirect('/students');
     }
 
     /**

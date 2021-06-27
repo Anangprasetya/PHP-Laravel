329cc9282cd3e909174fe0e13950ed3d470ea338
Anang <tyoandini@gmail.com>
2021-06-27 09:34:23 +0700

    menambah old value di form tambah data

--- resources/views/students/create.blade.php
+++ resources/views/students/create.blade.php
@@ -10,21 +10,21 @@
                 @csrf
                 <div class="mb-3">
                     <label for="nama" class="form-label">Nama</label>
-                    <input type="text" class="form-control @error('nama') is-invalid @enderror" id="nama" placeholder="Masukkan Nama" name="nama">
+                    <input type="text" class="form-control @error('nama') is-invalid @enderror" id="nama" placeholder="Masukkan Nama" name="nama" value="{{old('nama')}}">
                     <div id="validationServer03Feedback" class="invalid-feedback">@error('nama') {{$message}} @enderror</div>
                 </div>
                 <div class="mb-3">
                     <label for="nrp" class="form-label">NRP</label>
-                    <input type="text" class="form-control @error('nrp') is-invalid @enderror" id="nrp" placeholder="Masukkan NRP" name="nrp">
+                    <input type="text" class="form-control @error('nrp') is-invalid @enderror" id="nrp" placeholder="Masukkan NRP" name="nrp" value="{{old('nrp')}}">
                     <div id="validationServer03Feedback" class="invalid-feedback">@error('nrp') {{$message}} @enderror</div>
                 </div>
                 <div class="mb-3">
                     <label for="email" class="form-label">Email</label>
-                    <input type="email" class="form-control" id="email" placeholder="Masukkan Email" name="email">
+                    <input type="email" class="form-control" id="email" placeholder="Masukkan Email" name="email" value="{{old('email')}}">
                 </div>
                 <div class="mb-3">
                     <label for="jurusan" class="form-label">Jurusan</label>
-                    <input type="text" class="form-control" id="jurusan" placeholder="Masukkan Jurusan" name="jurusan">
+                    <input type="text" class="form-control" id="jurusan" placeholder="Masukkan Jurusan" name="jurusan" value="{{old('jurusan')}}">
                 </div>
 
                 <button type="submit" class="btn btn-info">Tambah Data</button>
